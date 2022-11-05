9cb234993073d8ec1cc138ae9ceaef891959e2d6
Denis19850204 <dg04021985@gmail.com>
2022-11-05 17:27:04 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,6 +42,7 @@ class App extends Component {
     ) {
       return alert(`${name}is already in contacts`);
     }
+    
 
     this.setState(prevState => ({
       contacts: [newContact, ...prevState.contacts],
